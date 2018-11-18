f64ce459105331d6f6453dccae7651a42826f246
vuvanlinh <vuvanlinh95@gmail.com>
2018-11-18 20:53:25 +0700

    tim uoc so chung lon nhat

---
+++ Ngon ngu lap trinh java/Tim uoc so chung lon nhat/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a, b;
+        Scanner nhap = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = nhap.nextInt();
+        System.out.println("Enter b: ");
+        b = nhap.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0)
+            System.out.println("No greatest common factor");
+
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+
+        System.out.println("Greatest common factor: " + a);
+    }
+}
